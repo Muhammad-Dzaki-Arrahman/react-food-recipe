c114b5a730c0212c1803ed1689ebdf019f4ce9c3
Muhammad-Dzaki-Arrahman <kiarrahman05@gmail.com>
2025-01-17 19:03:59 +0700

    food recipe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+function App() {
+  const url = "https://spoonacular.com/food-api/docs";
+  return (
+    <>
+      <Navbar />
+      <div className="">
+        <h1>Hello, It's Me!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
